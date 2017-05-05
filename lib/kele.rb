093148866ecb5ee9b9377a17412365f0cd6b626c
Stephen Vancott <thevanicotti@gmail.com>
2017-05-05 12:14:04 -0400

    Initalize and authorize KeleClient

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,2 +1,11 @@
+require 'httparty'
+
 class Kele
-  def initialize
+  include HTTParty
+
+  def initialize(email, password)
+
+    @base_url = 'https://www.bloc.io/api/v1'
+    @auth_token = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
+  end
+end
